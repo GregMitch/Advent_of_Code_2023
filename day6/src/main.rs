b238aa116bc48c5b1f6d623a0f902924686fcac6
GregMitch <grmitch1@gmail.com>
2023-12-06 20:52:18 -0500

    Day6 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -12,42 +12,34 @@ fn main() {
     aoc_parser.advance();
     let mut time_string: String = aoc_parser.line_buffer.clone();
     let pos: usize = time_string.chars().position(|c: char| c == ':').unwrap();
-    time_string.replace_range(..pos+2, "");        
+    time_string.replace_range(..pos+2, "");
+    time_string.retain(|c| !c.is_whitespace());        
     
-    let times: Vec<i32> = time_string
-        .split_whitespace()
-        .filter_map(|w| w.parse().ok())
-        .collect();
+    let time: f64 = time_string.parse().unwrap();
 
     aoc_parser.advance();
     let mut distance_string: String = aoc_parser.line_buffer.clone();
     let pos: usize = distance_string.chars().position(|c: char| c == ':').unwrap();
-    distance_string.replace_range(..pos+2, "");        
+    distance_string.replace_range(..pos+2, "");
+    distance_string.retain(|c| !c.is_whitespace()); 
         
-    let records: Vec<i32> = distance_string
-        .split_whitespace()
-        .filter_map(|w| w.parse().ok())
-        .collect();
+    let record: f64 = distance_string.parse().unwrap();
 
     drop(aoc_parser);
+    println!("{}", time);
+    println!("{}", record);
 
-    println!("{:?}", times);
-    println!("{:?}", records);
-    let mut possible_wins: Vec<i32> = Vec::new();
-    for i in 0..times.len() {
-        let mut count: i32 = 0;
-        for j in 1..times[i] {
-            let x = (times[i] - j) * j;
-            if x > records[i] {count += 1;}
-            //println!("x: {}, record: {}", x, records[i]);
-        }
-        possible_wins.push(count);
-    }
-    println!("{:?}", possible_wins);
+    //Quadratic formula time: (-b +/- sqrt(b^2 - 4ac)) / 2a
+    //a = -1
+    //b = time
+    //c = -record
+   
+    let num_to_root: f64 = (time*time) - 4.0*(-1.0)*(record*-1.0);
+    let root1: f64 = ((time*(-1.0)) - num_to_root.sqrt())/ (2.0*(-1.0));
+    let root2: f64 = ((time*(-1.0)) + num_to_root.sqrt())/ (2.0*(-1.0));
 
-    let mut res: i32 = 1;
-    for value in possible_wins {
-        res = res * value;
-    }
-    println!("{}", res);
+    let root1_int: i64 = root1.round() as i64;
+    let root2_int: i64 = root2.round() as i64;
+
+    println!("{}", root1_int - root2_int + 1);
 }
