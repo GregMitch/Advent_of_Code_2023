b7a20e9f85f6c73d14fb37cc3698585fe1b950f6
GregMitch <grmitch1@gmail.com>
2023-12-09 06:29:59 -0500

    Day8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::ffi::OsString;
 use std::collections::HashMap;
+use num::integer::lcm;
 mod parser;
 
 fn main() {
@@ -23,8 +24,9 @@ fn main() {
         }
     }
 
-    //Make map of Nodes and where they lead to
+    //Make map of Nodes and where they lead to, as well as Vector of Source Nodes (any that end with A)
     let mut node_map: HashMap<String,[String;2]> = HashMap::new();
+    let mut source_nodes: Vec<String> = Vec::new();
     aoc_parser.advance();
     let mut end_of_file: bool = false;
     while !end_of_file {
@@ -34,6 +36,11 @@ fn main() {
         let node_info: Vec<&str> = node_string.split('=').collect();
 
         let node_name: &str = node_info[0].trim();
+
+        if node_name.ends_with('A') {
+            source_nodes.push(node_name.to_string());
+        }
+
         let mut node_destinations: String = node_info[1].to_string();
         node_destinations.retain(|c| !c.is_whitespace() && c != '(' && c != ')');
         let node_destinations_formatted: Vec<&str> = node_destinations.split(',').collect();
@@ -43,17 +50,31 @@ fn main() {
         if !aoc_parser.has_more_lines() { end_of_file = true; }
     }
 
-    //Loop through left or right instructions starting from AAA until ZZZ
-    let mut res:i64 = 0;
-    let mut left_right_index: usize = 0;
-    let mut current_node: String = String::from("AAA");
-    while current_node != String::from("ZZZ") {
-        let direction: usize = left_or_right[left_right_index];
-        current_node = node_map.get(&current_node).unwrap()[direction].clone();
-        res += 1;
-        left_right_index += 1;
-        if left_right_index == left_or_right.len() { left_right_index = 0; }
+    //Loop through left or right instructions starting from ??A until ??Z, for all Source Nodes
+    //Add Number of steps to Vector
+    let mut steps: Vec<i64> = Vec::new();
+    for node in source_nodes {
+        let mut current_node: String = node;
+        let mut left_right_index: usize = 0;
+        let mut step:i64 = 0;
+        
+        while !current_node.ends_with('Z') {
+            let direction: usize = left_or_right[left_right_index];
+            current_node = node_map.get(&current_node).unwrap()[direction].clone();
+            step += 1;
+            left_right_index += 1;
+            if left_right_index == left_or_right.len() { left_right_index = 0; }
+        }
+        steps.push(step);
     }
-    println!("{}",res);
+    
+    //Get the Least Common Multiple, I just used a crate :p
+    let mut res: i64 = 1;
+    for value in steps {
+        res = lcm(res,value);
+    }
+
+    println!("{}", res);
     
 }
+ 
